0e44c8818d3fe7c12490d8d008b2b84e342cfdb0
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-06-15 12:05:54 +0900

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // 1 unit in Unity = 1 meter in real world, 1 meter/frame -> 60fps -> 60 mps
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime); // 5 m/s
+        // transform.Translate(new Vector3(-1, 0, 0));
+
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,
+            -4.48f, 4.48f), 0);
+
+        if (transform.position.x > 9.4f)
+        {
+            transform.position = new Vector3(-9.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.4f)
+        {
+            transform.position = new Vector3(9.4f, transform.position.y, 0);
+        }
+    }
+}
